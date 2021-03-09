6dd0e42b8e8ba3f34328289b2ed1baf6039978bd
Lazaraaus <wood.yjaden@gmail.com>
2021-03-08 19:03:04 -0500

    Updated settings for staticfiles, Added Bulma stylings to user-auth flow, and About page + tests

--- pages/tests.py
+++ pages/tests.py
@@ -3,7 +3,7 @@
 # IMPORTS 
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomepageTests(SimpleTestCase):
 
@@ -23,7 +23,7 @@ class HomepageTests(SimpleTestCase):
         )
     def test_homepage_contains_correct_html(self):
         self.assertContains(
-            self.response, 'Homepage'
+            self.response, 'Bookstore Home'
         )
 
     def test_homepage_url_resolves_homepageview(self):
@@ -34,4 +34,35 @@ class HomepageTests(SimpleTestCase):
             view.func.__name__,
             HomePageView.as_view().__name__
         )
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! This should not exist on the page.'
+        )
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(
+            self.response, 'About Page'
+        )
+
+    def test_homepage_url_resolves_homepageview(self):
+        # Get view used to resolve homepage url 
+        view = resolve('/about/')
+        # Check if resolved view and HomePageView are equal 
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
+
 # Create your tests here.
